07d369b170f9308686fd034d944deaf80c20df77
abel <abelcheruvathoor@gmail.com>
2019-12-16 11:58:19 +0530

    Completed Clima App

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/networking.dart';
+import '../services/location.dart';
+
+const apiKey = 'c41cbe868d42f2a07e05929bdbf78a15';
+
+const openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        "$openWeatherMap?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric");
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future<dynamic> getCityWeather(String cityName) async{
+    NetworkHelper networkHelper=NetworkHelper(
+      "$openWeatherMap?q=$cityName&appid=$apiKey&units=metric"
+    );
+    var weatherData=await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
